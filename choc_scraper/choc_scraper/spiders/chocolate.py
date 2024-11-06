dae4b166b3cbc609f8b4059b91f36767f725a58a
usman-jehangir-devsinc <usman.jehangir@devsinc.com>
2024-11-07 03:28:22 +0500

    Chocolate scraper complete

--- choc_scraper/choc_scraper/spiders/chocolate.py
+++ choc_scraper/choc_scraper/spiders/chocolate.py
@@ -1,10 +1,39 @@
 import scrapy
+import re
 
 
 class ChocolateSpider(scrapy.Spider):
     name = "chocolate"
     allowed_domains = ["chocolate.co.uk"]
     start_urls = ["https://www.chocolate.co.uk/collections/all"]
+    base_url = "https://www.chocolate.co.uk"
 
     def parse(self, response):
-        pass
+        products = response.css('product-item')
+
+        for product in products:
+            #here we put the data returned into the format we want to output for our csv or json file
+            print("-" * 30)
+            print(product.css('span.price').get())
+            price = re.search(".\d+(\.\d+)?", product.css('span.price').get()).group()
+            # print({
+            #     'name' : product.css('a.product-item-meta__title::text').get(),
+            #     'price' : price,
+            #     'url' : product.css('div.product-item-meta a').attrib['href'],
+            # })
+            print(price)
+            print("-" * 30)
+            yield{
+                'name' : product.css('a.product-item-meta__title::text').get(),
+                'price' : price,
+                'url' : self.base_url + product.css('div.product-item-meta a').attrib['href'],
+            }
+
+        next_page = response.css('[rel="next"] ::attr(href)').get()
+
+        if next_page:
+            next_url = self.base_url + next_page
+            yield response.follow(next_url, callback=self.parse)
+
+
+
